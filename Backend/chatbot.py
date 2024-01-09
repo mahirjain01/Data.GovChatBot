be299a3d379e1a258616cb563e2dab1dd134fe49
Mahir Jain <95163158+mahirjain01@users.noreply.github.com>
2024-01-09 05:36:10 +0530

    Update chatbot.py

--- Backend/chatbot.py
+++ Backend/chatbot.py
@@ -1,12 +1,12 @@
 from langchain_experimental.agents.agent_toolkits import create_csv_agent
 from langchain.llms import OpenAI
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env
+load_dotenv()
 
 def create_chatbot(csv_file_path, user_question):
-    # Set the OpenAI API key as an environment variable
-    openai_api_key = "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"
-    os.environ["OPENAI_API_KEY"] = openai_api_key
-
     try:
         openai_instance = OpenAI() 
         agent = create_csv_agent(openai_instance, csv_file_path, verbose=True)
